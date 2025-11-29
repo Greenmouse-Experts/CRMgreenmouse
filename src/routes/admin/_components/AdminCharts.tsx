46864519788b11f24abaddd0f00feaa16b45fb03
destiny <desto4q@gmail.com>
2025-11-29 23:34:32 +0100

    colors

--- src/routes/admin/_components/AdminCharts.tsx
+++ src/routes/admin/_components/AdminCharts.tsx
@@ -13,11 +13,11 @@ export default function AdminCharts() {
   return (
     <div className="min-h-[300px] w-full   grid lg:grid-cols-3 gap-4 py-4 px-6">
       <div className="  col-span-3 lg:col-span-2  space-y-8 ">
-        <div className="h-[250px] bg-primary rounded-xl shadow flex items-center  text-primary-content px-8 relative w-full text-white">
-          <div className="bg-secondary/20  text-white font-bold rounded-md p-2 text-sm top-0 left-0 m-6 absolute">
+        <div className="h-[200px] bg-primary rounded-xl shadow flex items-center  text-primary-content px-8 relative w-full ">
+          <div className="bg-secondary/20   text-white font-bold rounded-md p-2 text-sm top-0 left-0 m-6 ml-8 absolute">
             {currentDate}
           </div>
-          <div className="space-y-4">
+          <div className="space-y-4 mt-5">
             <h2 className="text-4xl font-bold">Welcome, Admin</h2>
             <p className="">Have a great day</p>
           </div>
