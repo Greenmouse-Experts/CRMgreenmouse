fba8118362a7ec1575ace0bec3b2bfd1176336d3
destiny <desto4q@gmail.com>
2025-12-04 09:02:05 +0100

    transactions

--- src/routes/admin/_components/AdminCharts.tsx
+++ src/routes/admin/_components/AdminCharts.tsx
@@ -11,7 +11,7 @@ export default function AdminCharts() {
   });
 
   return (
-    <div className="min-h-[300px] w-full   grid lg:grid-cols-3 gap-4 py-4 px-6">
+    <div className="min-h-[300px] w-full   grid lg:grid-cols-3 gap-4 py-4 ">
       <div className="bg-primary rounded-xl shadow flex flex-col justify-center text-primary-content p-6 sm:p-8 relative w-full col-span-3 min-h-[180px] sm:min-h-[200px]">
         <div className="space-y-2 sm:space-y-4">
           <div className="bg-secondary/20 text-white font-bold rounded-md p-2 text-xs sm:text-sm w-fit">
