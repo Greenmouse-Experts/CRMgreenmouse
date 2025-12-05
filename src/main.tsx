3659ed2a9096822a95d72d2a0f869cafce9f0b1e
destiny <desto4q@gmail.com>
2025-12-05 17:31:00 +0100

    dashboard stats, fonts

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,9 @@ import { RouterProvider, createRouter } from "@tanstack/react-router";
 
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
-import "./index.css";
 import "./styles.css";
+
+import "./index.css";
 import reportWebVitals from "./reportWebVitals.ts";
 
 // Create a new router instance
