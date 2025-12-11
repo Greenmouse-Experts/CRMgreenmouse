fb024b1e8bba896e0768aa884367f9fbf9fce87c
destiny <desto4q@gmail.com>
2025-12-11 18:39:52 +0100

    subscriptions update

--- src/routes/admin/users/permissions/index.tsx
+++ src/routes/admin/users/permissions/index.tsx
@@ -1,9 +1,55 @@
+import PageHeader from "@/components/Headers/PageHeader";
 import { createFileRoute } from "@tanstack/react-router";
+import ActionButton from "@/components/buttons/ActionButton";
+import { PlusCircleIcon } from "lucide-react";
+import SimpleContainer from "@/components/SimpleContainer";
+import CustomTable from "@/components/tables/CustomTable";
+import { faker } from "@faker-js/faker";
+import ContainerRow from "@/components/ContainerRow";
+import { useSearch } from "@/stores/data";
 
 export const Route = createFileRoute("/admin/users/permissions/")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <div></div>;
+  const generatePermissions = (count: number) => {
+    const permissions = [];
+    for (let i = 0; i < count; i++) {
+      permissions.push({
+        id: faker.string.uuid(),
+        name: faker.lorem.words(2),
+        description: faker.lorem.sentence(),
+        createdAt: faker.date.past().toLocaleDateString(),
+      });
+    }
+    return permissions;
+  };
+  const props = useSearch();
+  const permissionsData = generatePermissions(10);
+
+  const columns = [
+    { key: "name", label: "Permission Name" },
+    { key: "description", label: "Description" },
+    { key: "createdAt", label: "Created At" },
+  ];
+
+  return (
+    <>
+      <PageHeader
+        title="Permissions"
+        description="Manage user roles and permissions."
+      >
+        <ActionButton title="Add Permission">
+          <PlusCircleIcon className="w-5 h-5" />
+          Add Permission
+        </ActionButton>
+      </PageHeader>
+
+      <SimpleContainer title="Permissions">
+        <ContainerRow searchProps={props} showSearch></ContainerRow>
+        <CustomTable data={permissionsData} columns={columns} />
+      </SimpleContainer>
+    </>
+  );
 }
